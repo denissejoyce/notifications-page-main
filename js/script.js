a3e68b8aa789ffc77916f550c2c1abd6d0b9a1d4
denissejoyce <denisse.alido@gmail.com>
2025-02-08 16:07:58 -0600

    desktop ready 🚀

--- js/script.js
+++ js/script.js
@@ -1,5 +1,29 @@
 "use strict";
 
-function markAsRead() {
-  alert("hi!");
+const count = document.getElementsByClassName("notifications__count")[0];
+const newAlerts = document.getElementsByClassName("alert");
+const alerts = document.getElementsByClassName("alert__activity");
+
+let alertCount = 3;
+
+function readAll() {
+  for (const newAlert of newAlerts) {
+    newAlert.classList.remove("alert--new");
+  }
+  for (const alert of alerts) {
+    alert.classList.remove("new");
+  }
+  alertCount = 0;
+  count.textContent = alertCount;
+}
+
+let num = 0;
+for (const newAlert of newAlerts) {
+  newAlert.addEventListener("click", function () {
+    newAlert.classList.remove("alert--new");
+    alerts[num].classList.remove("new");
+    num++;
+    alertCount--;
+    alertCount < 0 ? (count.textContent = 0) : (count.textContent = alertCount);
+  });
 }
